0ee7e25ba6205fd4f55710570d9c60f260c31dcc
ayaup <shkaodao@icloud.com>
2020-11-22 09:14:27 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'wangker';
         $user->email = 'wangker@test.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
